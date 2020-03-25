285ce9de37a73629fc4f7ddcebf220cf0aa1cee8
senspond20 <senspond@gmail.com>
2020-03-25 19:55:10 +0900

    update

--- main.js
+++ main.js
@@ -1,40 +1,42 @@
 
 // start-window 
-window.onload = function() {menu_view(1),sidemenu_view(1)};
+window.onload = function() {menu_view(1),sidemenu_view(1),change_sideTitle(1)};
+
+// navbar 
+var navbar1 = document.getElementById('navbar1');
+var navbar2 = document.getElementById('navbar2');
+var navbar3 = document.getElementById('navbar3');
+var navbar4 = document.getElementById('navbar4');
+var navbar5 = document.getElementById('navbar5');
 
 //addEventListener
-document.getElementById('navbar1').addEventListener('click' ,function(){menu_view(1)});
-document.getElementById('navbar1').addEventListener('click' ,function(){sidemenu_view(1)});
-
-document.getElementById('navbar2').addEventListener('click' ,function(){menu_view(2)});
-document.getElementById('navbar2').addEventListener('click' ,function(){sidemenu_view(2)});
-
-document.getElementById('navbar3').addEventListener('click' ,function(){menu_view(3)});
-document.getElementById('navbar3').addEventListener('click' ,function(){sidemenu_view(3)});
-
-document.getElementById('navbar4').addEventListener('click' ,function(){menu_view(4)});
-document.getElementById('navbar4').addEventListener('click' ,function(){sidemenu_view(4)});
-
-document.getElementById('navbar5').addEventListener('click' ,function(){menu_view(5)});
-document.getElementById('navbar5').addEventListener('click' ,function(){sidemenu_view(5)});
+navbar1.addEventListener('click' ,function(){menu_view(1),sidemenu_view(1),change_sideTitle(1)});
+navbar2.addEventListener('click' ,function(){menu_view(2),sidemenu_view(2),change_sideTitle(2)});
+navbar3.addEventListener('click' ,function(){menu_view(3),sidemenu_view(3),change_sideTitle(3)});
+navbar4.addEventListener('click' ,function(){menu_view(4),sidemenu_view(4),change_sideTitle(4)});
+navbar5.addEventListener('click' ,function(){menu_view(5),sidemenu_view(5),change_sideTitle(5)});
 
 
 /*======= function ========= */
 var old_viewid = 0;
 var old_sviewid = 0;
 
-function menu_view(viewid){
+function change_sideTitle(viewid){
+    var title = document.getElementById("navbar"+viewid);
+    document.getElementById("sideTitle").textContent = title.textContent;
+}
 
+function menu_view(viewid){
     if(viewid!=old_viewid){
     document.getElementById("submenu"+viewid).style.display="block";
 
     if(old_viewid!="0")document.getElementById("submenu"+old_viewid).style.display="none";
     old_viewid = viewid;
     }
+    
 }
 
 function sidemenu_view(viewid){
-
     if(viewid!=old_sviewid){
     document.getElementById("sidemenu"+viewid).style.display="block";
 
@@ -43,9 +45,10 @@ function sidemenu_view(viewid){
     }
 }
 
-/*   function menu_hide(hideid){
-        document.getElementById("submenu"+hideid).style.display="none";
-        old_viewid = 0;
-    
-    }*/
+// function menu_hide(hideid){
+//     document.getElementById("submenu"+hideid).style.display="none";
+//     // document.getElementById("sub-bar").style.backgroundColor ="none";
+//     old_viewid = 0;
+
+// }
 
